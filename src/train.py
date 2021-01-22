c3e68783de1ee402438f472b24e04ce0e86eb999
unknown <Pratik Asarkar@PratikA-GGN.gle.in>
2021-01-23 01:49:43 +0530

    done for the night

--- src/train.py
+++ src/train.py
@@ -1,1 +1,9 @@
-print("Hello world")
+from sklearn import preprocessing
+import pandas as pd
+
+if __name__ == "__main__":
+    df = pd.read_csv(TRAINING_DATA)
+    train_df = df[df['kfold'].isin(FOLD_MAPPING.get(FOLD))]
+    valid_df = df[df['kfold'] == FOLD]
+    
+    
